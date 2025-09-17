3eabc80703d6f6f4fbe47e067d528bebe6418978
jorge221317-max <jorge221317@hotmail.com>
2025-09-16 23:59:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Lista de tareas (en memoria)
+# Lista de tareas en memoria
 todos = []
 
 @app.route("/")
