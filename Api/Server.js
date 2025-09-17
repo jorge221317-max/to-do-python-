4dceb7d1d8f126509a57e9bcc7d4c03e3543a50e
jorge221317-max <jorge221317@hotmail.com>
2025-09-17 18:33:47 -0300

    Update Server.js

--- Api/Server.js
+++ Api/Server.js
@@ -1,19 +1,17 @@
-import express from "express";
-import cors from "cors";
-import tasksRouter from "./routes/tasks.js";
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API To-Do funcionando 🚀");
-});
+// ejemplo de ruta
+app.get('/', (req, res) => res.send('Hello Render!'));
 
-app.use("/api/tasks", tasksRouter);
+// usar el puerto que Render asigna o fallback 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
