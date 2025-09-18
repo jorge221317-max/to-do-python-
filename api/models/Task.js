05ff0c7b9f4da9904c23bb5a004a0606a477972d
jorge221317-max <jorge221317@hotmail.com>
2025-09-18 01:15:55 -0300

    Update Task.js

--- api/models/Task.js
+++ api/models/Task.js
@@ -1,20 +1,9 @@
-// api/models/Task.js
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-}, {
-  timestamps: true
-});
+  title: { type: String, required: true, trim: true },
+  completed: { type: Boolean, default: false }
+}, { timestamps: true });
 
 const Task = mongoose.model("Task", taskSchema);
-
 export default Task;
