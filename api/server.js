f198fd701da3a481cba3ee3c796cc7083d73b161
jorge221317-max <jorge221317@hotmail.com>
2025-09-17 22:56:18 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,6 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import taskRoutes from './routes/taskRoutes.js';
-
-dotenv.config();
-const app = express();
-app.use(express.json());
-
-app.use('/api/tasks', taskRoutes);
-
 const PORT = process.env.PORT || 5000;
+const HOST = "0.0.0.0"; // IMPORTANTE para Render
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log('MongoDB conectado');
-    app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-  })
-  .catch(err => console.error(err));
+app.listen(PORT, HOST, () => {
+  console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
+});
