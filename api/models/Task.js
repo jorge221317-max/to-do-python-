59b31818ea832e38d06a7df5979a5ecafd5fbdc5
jorge221317-max <jorge221317@hotmail.com>
2025-09-18 13:33:48 -0300

    Delete api directory

--- api/models/Task.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const taskSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("Task", taskSchema);
