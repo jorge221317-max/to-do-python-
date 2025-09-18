99b1cff658deabbe24889cfd8cb65a62a8ffd3c0
jorge221317-max <jorge221317@hotmail.com>
2025-09-18 13:58:38 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const PORT = process.env.PORT || 10000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+});
