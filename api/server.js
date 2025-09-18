efa9be687961fff0e11be4c8c6306cc10dc3518c
jorge221317-max <jorge221317@hotmail.com>
2025-09-17 23:05:07 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,13 @@
+import express from "express";
+import taskRoutes from "./routes/taskRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/tasks", taskRoutes);
+
 const PORT = process.env.PORT || 5000;
-const HOST = "0.0.0.0"; // IMPORTANTE para Render
+const HOST = "0.0.0.0";
 
 app.listen(PORT, HOST, () => {
   console.log(`Servidor corriendo en http://${HOST}:${PORT}`);
