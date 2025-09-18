c22f5831e0cc0b2073ba660ccb78f7be09052528
jorge221317-max <jorge221317@hotmail.com>
2025-09-18 01:17:50 -0300

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -1,28 +1,10 @@
-// api/models/User.js
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-});
+  username: { type: String, required: true, unique: true, trim: true },
+  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
-
 export default User;
