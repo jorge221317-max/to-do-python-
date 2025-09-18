8059286ff6467a2e45eb36bd74b22b5430218ccd
jorge221317-max <jorge221317@hotmail.com>
2025-09-18 00:02:05 -0300

    Update task.js

--- api/models/task.js
+++ api/models/task.js
@@ -1,9 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-  user: { type: String, required: true } // ID del usuario que creó la tarea
-}, { timestamps: true });
+  completed: { type: Boolean, default: false }
+});
 
-export default mongoose.model('Task', taskSchema);
+export default mongoose.model("Task", taskSchema);
