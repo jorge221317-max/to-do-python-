efb995e50265034ae9a50b9dc0b7015e4a461c74
jorge221317-max <jorge221317@hotmail.com>
2025-09-18 00:45:37 -0300

    Update and rename task.js to Task.js

--- api/models/task.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const taskSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  completed: { type: Boolean, default: false }
-});
-
-export default mongoose.model("Task", taskSchema);
