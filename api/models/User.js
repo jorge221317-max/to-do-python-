b856b560ed7d64f8f31db35a89ac75a7f8e4c604
jorge221317-max <jorge221317@hotmail.com>
2025-09-18 03:26:11 -0300

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true, trim: true },
-  email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String, required: true }
-}, { timestamps: true });
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+});
 
-const User = mongoose.model("User", userSchema);
-export default User;
+// Hashear password antes de guardar
+userSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+export default mongoose.model("User", userSchema);
